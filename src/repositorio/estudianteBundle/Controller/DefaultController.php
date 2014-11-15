98efdee99b01373e9272f571a28d66a583058d81
david <dasilva6793@gmail.com>
2014-11-15 08:13:07 -0500

    15/11/2014

--- src/repositorio/estudianteBundle/Controller/DefaultController.php
+++ src/repositorio/estudianteBundle/Controller/DefaultController.php
@@ -17,31 +17,37 @@ class DefaultController extends Controller
         $repositoriys = $this->getDoctrine()->getRepository("estudianteBundle:Sede");
         $sedes = $repositoriys->findAll();
 
-        return $this->render('estudianteBundle::estudiante.html.twig',array('programas'=>$programas,'sedes'=>$sedes,));
+        return $this->render('estudianteBundle::estudiante.html.twig',array('programas'=>$programas,'sedes'=>$sedes));
     }
 
     public function registrarAction()
     {   
+        try {
         
-        $programa = new ProgramaAcademico();
-        $programa->setCodigoProg($_POST['progaca']);
+        $codigo = $_POST['codigo'];
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        $correo = $_POST['correo'];
+        $codigosede = $_POST['sede'];
+        $codigoprog = $_POST['progaca'];
 
-        $sede = new Sede();
-        $sede->setCodigoSede($_POST['sede']);
-
-        $usuario = new Usuario();
-    	$usuario->setCodigo($_POST['codigo']);
-    	$usuario->setNombre($_POST['nombre']);
-    	$usuario->setApellido($_POST['apellido']);
-    	$usuario->setCorreo($_POST['correo']);
-    	$usuario->setCodigoProg($programa);
-    	$usuario->setCodigoSede($sede);
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($usuario);
-        $em->flush();
-
+        $con= $this->getDoctrine()->getConnection();
+        $sql= "INSERT INTO `usuario` (`codigo`, `nombre`, `apellido`, `correo`, `codigo_sede`, `codigo_prog`) VALUES
+        ('$codigo','$nombre','$apellido','$correo','$codigosede','$codigoprog')";
+                
+        $insert = $con->query($sql);
         
-        return $this->render('estudianteBundle::registrat.html.twig');
+        return $this->render('estudianteBundle::registrar.html.twig',array('insert'=>$sql));
+                            
+        } catch (\Exception $e) {
+        
+            echo"<script>alert('Usted esta siendo redireccionado a la pagina principal')</script>";
+            
+        }finally{
+
+            
+
+        }
+        
     }
 }
---
+++ src/repositorio/estudianteBundle/Resources/public/js/base.js
@@ -0,0 +1,5 @@
+function mostrarerror (codigo) {
+	if (codigo == 0) {
+		alert("Codigo ya se encuentra en la base de datos");
+	};
+}
